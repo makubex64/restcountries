b3d1f2963ccfa326e5ccd80d31cb8d6b55c6dabb
Jorge Adarmes <jorgeluismakubex@gmail.com>
2023-02-22 15:06:12 -0430

    simple change

--- src/routes/NestedRoutes.jsx
+++ src/routes/NestedRoutes.jsx
@@ -1,14 +1,14 @@
-import {createHashRouter} from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import CountryID from '../components/CountryID'
 import ErrorPage             from '../components/ErrorPage'
 import Root                  from './Root'
 import App                   from '../App'
 import Search                from '../components/Search'
 
-export const router = createHashRouter([
+export const router = createBrowserRouter([
 
     {
-      path:{process.env.PUBLIC_URL + '/'}, 
+      path:"/", 
       element: <Root/> ,
       errorElement: <ErrorPage/>,
 
